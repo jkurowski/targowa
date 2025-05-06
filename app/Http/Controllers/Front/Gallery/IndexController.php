8147df8dbcaa8aaf160e9e5dd71e33297244c7af
Jacek Kurowski <jacek@4dl.pl>
2025-05-07 01:25:16 +0200

    Strona glowna - RWD

--- app/Http/Controllers/Front/Gallery/IndexController.php
+++ app/Http/Controllers/Front/Gallery/IndexController.php
@@ -5,14 +5,21 @@ namespace App\Http\Controllers\Front\Gallery;
 use App\Http\Controllers\Controller;
 use App\Models\Gallery;
 use App\Models\Page;
+use App\Repositories\GalleryRepository;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    private $repository;
+    public function __construct(GalleryRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function index()
     {
         return view('front.gallery.index', [
-            'galleries' => Gallery::all(),
+            'galleries' => $this->repository->allSort('ASC'),
             'page' => Page::where('id', 7)->first(),
         ]);
     }
